c2e270b304b6b28c7bddeaa581d93474a55897a1
sushiiyay <nfa.aliya7811@gmail.com>
2026-03-02 14:58:19 +0700

    //library

---
+++ PraktikumPemdas2.cpp
@@ -0,0 +1,14 @@
+//library
+#include <iostream>
+using namespace std;
+
+//deklarasi variabel global
+int phi, r;
+
+//implementasi fungsi dan prosedur
+void input (){
+    cout << "Masukkan Phi : ";
+    cin >> phi
+    cout << "Masukkan jari-jari : ";
+    cin >> r;
+}
