d33edd26d35c407b984074b35127fcc4dfb4ddc4
sushiiyay <nfa.aliya7811@gmail.com>
2026-03-02 15:04:29 +0700

    //deklarasi variabel global

--- PraktikumPemdas2.cpp
+++ PraktikumPemdas2.cpp
@@ -3,12 +3,3 @@
 using namespace std;
 
 //deklarasi variabel global
-int phi, r;
-
-//implementasi fungsi dan prosedur
-void input (){
-    cout << "Masukkan Phi : ";
-    cin >> phi
-    cout << "Masukkan jari-jari : ";
-    cin >> r;
-}
